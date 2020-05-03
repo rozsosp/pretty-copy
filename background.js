9f160a22e4537f21612cf0e0f3d5e8ba603ce76f
Péter Rozsos <peterrozsos@gmail.com>
2020-05-03 14:30:44 +0200

    Added context menu item
    Added a context menu item to copy the url.
    Using the execCommand solution, because the clipboard api had an error (document not focused).

---
+++ background.js
@@ -0,0 +1,22 @@
+const contextMenuItem = {
+  id: "copyText",
+  title: "Copy Current Page URL in the pretty way",
+  contexts: ["all"]
+};
+
+const copy = url => {
+  var textArea = document.createElement("textarea");
+  document.body.appendChild(textArea);
+  textArea.value = url;
+  textArea.select();
+  document.execCommand("copy");
+  document.body.removeChild(textArea);
+} 
+
+chrome.contextMenus.create(contextMenuItem);
+
+chrome.contextMenus.onClicked.addListener((data) => {
+  if (data.menuItemId === contextMenuItem.id) {
+    copy(data.pageUrl);
+  }
+});
